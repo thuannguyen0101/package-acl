ccef1ebba296237aa2a3217c0fe652af42a2222b
thuannguyen0101 <thuannguyen0101.work@gmail.com>
2024-11-15 18:22:41 +0700

    backup

---
+++ tests/BaseAuthTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+
+class BaseAuthTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    protected $user;
+    protected $token;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('migrate');
+        $this->artisan('db:seed', ['--class' => 'Workable\\UserTenant\\Database\\Seeders\\UserSeeder']);
+
+        $this->user = User::query()->where('email', 'thuannn@gmail.com')->first();
+    }
+
+    protected function login()
+    {
+        $response = $this->postJson(route('api.auth.login'), [
+            'login'    => 'thuannn',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(200);
+
+        $this->token = $response->json('data.token');
+
+        $this->withHeader('Authorization', 'Bearer ' . $this->token);
+    }
+}
